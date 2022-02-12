d827b4bde61e617c897250de544a0ea744876fc6
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-02-11 20:38:55 -0500

    created output file paths

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,7 +11,10 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      // [name] will inject main, install and cards '.bundle.js' and create 3 separate bundled files for each of our entry points along with any dependencies, packages or files that those entry points need
+      filename: '[name].bundle.js',
+      // this tells webpack to output it at the root of the 'dist' folder
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
